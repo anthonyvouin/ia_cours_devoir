a580b48f4bbcd348a95c20d38a28afee7a8105e7
akia <ch.royer15@gmail.com>
2024-12-21 10:54:40 +0100

    qcm

---
+++ src/interface/course.dto.ts
@@ -0,0 +1,39 @@
+export interface ContentPart {
+    subtitle: string;
+    content: string;
+}
+
+export interface CourseStep {
+    title: string;
+    content: ContentPart[];
+    to_remember: string;
+    resources: string;
+    parts?: { subtitle: string, content: string }[];
+}
+
+export interface CourseOutline {
+    title: string;
+    abstract: string;
+    duration: number;
+    pedagogical_objectives: string;
+}
+
+export interface Course {
+    name: string;
+    level: string;
+    duration: number;
+    description: string;
+    course_outline: CourseOutline[];
+    course_steps_content: CourseStep[];
+    is_censured: boolean;
+}
+
+
+export interface CourseList {
+    name: string,
+    id: number,
+    subject: string,
+    duration: number,
+    level: string,
+    slug: string
+}
